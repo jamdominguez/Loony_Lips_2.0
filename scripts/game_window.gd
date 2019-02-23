a48fea5160e0059d2ed4ec803c600dcd8092fd90
Jose Antonio Munoz Dominguez <jamdominguez@gmail.com>
2019-02-23 08:50:59 +0100

    - Font added to labels

---
+++ scripts/game_window.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+# Declare member variables here.
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$board/word_label.bbcode_text = "NEVERA"
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
