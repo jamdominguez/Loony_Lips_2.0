93d20c84ef251b2e01fe6aca59dcf64fd66ab083
Jose Antonio Munoz Dominguez <jamdominguez@gmail.com>
2019-03-10 08:24:49 +0100

    - log cleaned - fix function name

--- scripts/welcome.gd
+++ scripts/welcome.gd
@@ -6,7 +6,7 @@ func _ready():
 	$welcome_board/language_button.add_item('Espanol', 0)
 	$welcome_board/language_button.add_item('English', 1)
 
-func refresh_lables():
+func refresh_labels():
 	$welcome_board/welcome_label.text = global.strings.welcome_title
 	$welcome_board/start_button.text = global.strings.welcome_button
 	$welcome_board/language_label.text = global.strings.select_language_label
@@ -22,12 +22,11 @@ func _on_start_button_pressed():
 		print("ERROR opening scene: " + scene_path)
 
 # Signal: language button pressed behavior
-func _on_language_button_item_selected(ID):
-	print(String(ID))
+func _on_language_button_item_selected(ID):	
 	if ID == 0:
 		global.strings = global.get_json("res://data/strings_spanish.json")
 		global.words_library = global.get_json("res://data/words_spanish.json")
 	elif ID == 1:
 		global.strings = global.get_json("res://data/strings_english.json")
 		global.words_library = global.get_json("res://data/words_english.json")
-	refresh_lables()
+	refresh_labels()
